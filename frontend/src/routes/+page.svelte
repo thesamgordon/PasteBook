c6fe48ef41d323eee40e5897d2312e07afb55d6a
Sam <loudbooked@gmail.com>
2025-12-04 09:15:45 -0500

    chore: update dependencies

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-	import Content from "$lib/components/Content.svelte";
-	import Floater from "$lib/components/Floater.svelte";
-	import Navbar from "$lib/components/Navbar.svelte";
+  import Content from "$lib/components/Content.svelte";
+  import Floater from "$lib/components/Floater.svelte";
+  import Navbar from "$lib/components/Navbar.svelte";
 
-	const { data } = $props();
+  const { data } = $props();
 </script>
 
 <Navbar editable={true} />
